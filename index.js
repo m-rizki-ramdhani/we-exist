a130f7dbc3839f140b7dcd6f39fc502a24f4dc28
m-rizki-ramdhani <ccetsf@gmail.com>
2025-01-04 23:04:10 +0700

    add routes file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const appConfig = require('./config/core_config');
 const { sequelize } = require("./database/connection");
+const routes = require('./routes/route')
 
 const app = express()
 app.use(express.json())
@@ -15,9 +16,7 @@ sequelize
   })
 
 
-app.get('/', (req, res) => {
-    res.status(200).send('Sukses')
-})
+app.use('/', routes)
 
 app.listen(appConfig.port, () => {
     console.log(`Server is running on http://localhost:${appConfig.port}`)
